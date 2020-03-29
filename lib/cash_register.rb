b671fc3f068e6b94e247c2cda36a9f550a29a68e
Tom Banish <tombanish@gmail.com>
2020-03-29 20:56:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
      @total = @total - (@total * (discount/100.to_f))
      "After the discount, the total comes to $#{@total.to_i}."
    else
-     
+     "There is no discount to apply."
   end
 
   
